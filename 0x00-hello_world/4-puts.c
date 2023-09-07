e16f3e1ab9a88dc7da40d1790c0202a93e0da5d5
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-07 06:27:30 -0700

    my forth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 /**
  * main - Entry point
  * Description: A C proqram that prints with put function
- * Return: Always 0*/
-
-
-
+ * Return: Always 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
