3eb1125c37e658abcb73f57a9e5db06d43aa4cf6
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-07 06:24:15 -0700

    pro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C proqram that prints with put function
+ * Return: Always 0*/
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
