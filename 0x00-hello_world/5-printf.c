b5872d5482d605312589e45f8dddff30907e7c66
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-07 06:58:17 -0700

    my 5th

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * description: A C program print by printf function 
+ *
+ *return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
