44cd723c633e65711d94614280d0717715ce335b
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-08 12:30:47 -0700

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit number
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; i <= 9; i++)
+	{
+		printf("%i", digit)
+	}
+	printf("\n")
+	return (0);
+}
+
