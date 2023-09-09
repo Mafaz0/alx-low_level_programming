6f3e36af036eeba468bdcc4101ccdc1510efe9fd
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-09 00:27:01 -0700

    you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include<stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different combinations of two digit.
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int digit = 0, digit2;
+
+	while (digit <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit != digit2 && digit < digit2)
+			{
+				putchar(digit + 48);
+				putchar(digit2 + 48);
+
+				if (digit + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		putchar('\n');
+
+		return (0);
+	}
