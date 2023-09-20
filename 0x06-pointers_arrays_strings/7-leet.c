44f58200d2a38638ee4579bf83f08bf9093d6f36
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-20 11:47:34 -0700

    hyt

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encode a string
+ * @c: string
+ * Return: string that is encode
+*/
+
+char *leet(char *c)
+{
+	char *ch = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (ch);
+}
