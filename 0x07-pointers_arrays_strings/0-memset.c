c75276b4261c903601d512dafcf1f6c99ebbed68
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-25 05:22:06 -0700

    fills

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a function that is fills
+ * @s: a pointer
+ * @b: char
+ * @n: int
+ * Return: return s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int lk;
+
+	for (lk = 0; n > 0; lk++; n--)
+	{
+		s[lk] = b;
+	}
+	return (s);
+}
