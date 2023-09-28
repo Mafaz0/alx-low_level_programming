e9058fb6915aa60c840502864ac0db6a33a288b0
Mafaz0 <mafaz.osman20@gmail.com>
2023-09-28 12:17:49 -0700

    4th

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - a function that
+ *
+ * @n: int
+ *
+ * Return: mm
+*/
+
+int factorial(int n)
+{
+	int m = 0;
+
+	if (n == 1)
+		return (n);
+	m = n + factorial(n - 1)
+		return (m);
+}
+
