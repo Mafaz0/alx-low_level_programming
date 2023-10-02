9de0c2e08ca80687c048b498af08e7af2a82fa30
Mafaz0 <mafaz.osman20@gmail.com>
2023-10-02 14:36:54 -0700

    goo

--- 0x09-static_libraries/1-args.c
+++ 0x09-static_libraries/1-args.c
@@ -6,8 +6,7 @@
  * @argv: this is pointer
  * Return: 0 or 1
 */
-
-int main(int argc, char *argv[] _attribute_((unuse))
+int main(int argc, char *argv[] _attribute_((unused))
 		{
 		printf("%d\n", argc - 1);
 		return (0);
