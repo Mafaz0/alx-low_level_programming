97f82f254e295aee562dced9643a62930c465ce3
Mafaz0 <mafaz.osman20@gmail.com>
2023-10-02 14:18:14 -0700

    b

---
+++ 0x09-static_libraries/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that give the number of the command line
+ * @argc: intger
+ * @argv: this is pointer
+ * Return: 0 or 1
+*/
+
+int main(int argc, char *argv[] _attribute_((unuse))
+		{
+		printf("%d\n", argc - 1);
+		return (0);
+		}
