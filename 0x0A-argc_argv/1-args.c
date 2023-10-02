20cbb8b430e5919f747ad259f9e5304d38698fbf
Mafaz0 <mafaz.osman20@gmail.com>
2023-10-02 15:40:28 -0700

    yohooo

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that print th num of argument
+ * @argc: integer
+ * @argv: thus is pointer
+ * Return: 0 or 1
+*/
+
+int main(int argc, char *argv[] _attribute_((unsed))
+		{
+		printf("%d\n", argc - 1);
+		return (0);
+		}
