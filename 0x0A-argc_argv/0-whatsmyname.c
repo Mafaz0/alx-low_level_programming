b968fa502c602879b7d3ea06e71d50aef899c7b1
Mafaz0 <mafaz.osman20@gmail.com>
2023-10-02 13:51:12 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a function that to print
+ * @argc: integer
+ * @argv: integer
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
