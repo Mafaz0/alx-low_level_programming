fceb109e350432d9f1efb881527a5ab47bbe1b76
Mafaz0 <mafaz.osman20@gmail.com>
2023-10-03 13:42:52 -0700

    ww

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,27 @@
+#include "main.h"
+/**
+ * *_strdup - a function that
+ * @str: pointer
+ * Return: 0
+*/
+char *_strdup(char *str)
+{	int g, u = 0;
+	char *qu;
+
+	if (str == NULL)
+	return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	qu = malloc(size * sizeof(*str) + 1);
+
+	if (qu == 0)
+	return (NULL);
+	else
+	{
+	for (; g < size; g++)
+	qu[g] = str[g];
+	}
+	return (qu);
 
